52637fa0de63f9769ba50663bcf480751105a629
Smithoo <smithoo7646@gmail.com>
2022-03-14 03:55:38 +0900

    Set server (express with typescript)

---
+++ backend/src/app.ts
@@ -0,0 +1,45 @@
+import createError from 'http-errors';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import logger from 'jet-logger';
+import StatusCodes from 'http-status-codes';
+import router from './routes';
+
+const app = express();
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Show routes called in console during development
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Security (helmet recommended in express docs)
+if (process.env.NODE_ENV === 'production') {
+    app.use(helmet());
+}
+
+app.use('/', router);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    next(createError(StatusCodes.BAD_REQUEST));
+});
+
+app.use((err: Error, req, res, next) => {
+    logger.err(err, true);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: err.message,
+    });
+});
+
+export default app;
---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import logger from 'jet-logger';
+import app from './app';
+
+// Constants
+const serverStartMsg = 'Express server started on port: ';
+const port = process.env.PORT || 3000;
+
+// Start server
+app.listen(port, () => {
+    logger.info(serverStartMsg + port);
+});
