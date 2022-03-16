26d3a4e5f5f22d8c8695a4dff6fa1cd8e6708b83
Smithoo <smithoo7646@gmail.com>
2022-03-16 23:14:35 +0900

    Add urql

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import urql from '@urql/vue';
 import App from './App.vue';
 
 const app = createApp(App);
 
 app.use(ElementPlus);
+app.use(urql, {
+    url: 'http://localhost:2999/graphql',
+});
 
 app.mount('#app');
