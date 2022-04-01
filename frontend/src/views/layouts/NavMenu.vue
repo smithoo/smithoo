64e00363585fd7cf7149bf2b5d838d5158f70af8
Smithoo <smithoo7646@gmail.com>
2022-04-02 01:44:47 +0900

    Change nav style

--- frontend/src/views/layouts/NavMenu.vue
+++ frontend/src/views/layouts/NavMenu.vue
@@ -1,38 +1,12 @@
 <template>
     <q-drawer show-if-above v-model="drawerOpen" :width="200" :breakpoint="500">
-        <q-scroll-area class="fit nav-scroll">
-            <q-list padding class="menu-list">
-                <q-item clickable v-ripple>
-                    <q-item-section avatar>
-                        <q-icon name="o_inbox" />
-                    </q-item-section>
-
-                    <q-item-section> Inbox </q-item-section>
+        <q-scroll-area class="fit menu-list">
+            <q-list padding>
+                <q-item active clickable v-ripple dense class="menu-item q-pt-sm q-pb-sm">
+                    <q-icon name="o_home" size="18px" />
+                    <span class="q-ml-sm">Home</span>
                 </q-item>
-
-                <q-item active clickable v-ripple>
-                    <q-item-section avatar>
-                        <q-icon name="o_star" />
-                    </q-item-section>
-
-                    <q-item-section> Star </q-item-section>
-                </q-item>
-
-                <q-item clickable v-ripple>
-                    <q-item-section avatar>
-                        <q-icon name="o_send" />
-                    </q-item-section>
-
-                    <q-item-section> Send </q-item-section>
-                </q-item>
-
-                <q-item clickable v-ripple>
-                    <q-item-section avatar>
-                        <q-icon name="o_drafts" />
-                    </q-item-section>
-
-                    <q-item-section> Drafts </q-item-section>
-                </q-item>
+                <q-separator key="test" />
             </q-list>
         </q-scroll-area>
     </q-drawer>
@@ -66,7 +40,12 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.nav-scroll {
+.menu-list {
     border-right: 1px solid #ddd;
+    .menu-item {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+    }
 }
 </style>
